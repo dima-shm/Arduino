42c6d29881fa78a465385a3b0532eff5b2888a6c
Dima Shumanski <dima.shumanski@gmail.com>
2019-01-13 20:18:17 +0300

    Added Lab_5 — JADE

---
+++ Lab_5_JADE/HelloWorldAgent.java
@@ -0,0 +1,9 @@
+import jade.core.Agent;
+
+public class HelloWorldAgent extends Agent
+{
+    protected void setup()
+    {
+        System.out.println("Hello World! My name is "+getLocalName());
+    }
+}
