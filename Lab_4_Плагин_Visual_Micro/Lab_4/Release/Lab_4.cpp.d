fdae4bc3fae5261e83937d858bca0f56690846d9
Dima Shumanski <dima.shumanski@gmail.com>
2019-01-13 20:17:44 +0300

    Added Lab_4 — Plugin Visual Micro

---
+++ Lab_4_Плагин_Visual_Micro/Lab_4/Release/Lab_4.cpp.d
@@ -0,0 +1,13 @@
+C:\Users\dimas\AppData\Local\Temp\VMBuilds\Lab_4\uno\Release\Lab_4.cpp.o: \
+ C:\Users\dimas\AppData\Local\Temp\VMBuilds\Lab_4\uno\Release\Lab_4.cpp \
+ D:\Apps\arduino-1.8.7\hardware\arduino\avr\cores\arduino/arduino.h \
+ D:\Apps\arduino-1.8.7\hardware\arduino\avr\cores\arduino/binary.h \
+ D:\Apps\arduino-1.8.7\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ D:\Apps\arduino-1.8.7\hardware\arduino\avr\cores\arduino/WString.h \
+ D:\Apps\arduino-1.8.7\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ D:\Apps\arduino-1.8.7\hardware\arduino\avr\cores\arduino/Stream.h \
+ D:\Apps\arduino-1.8.7\hardware\arduino\avr\cores\arduino/Print.h \
+ D:\Apps\arduino-1.8.7\hardware\arduino\avr\cores\arduino/Printable.h \
+ D:\Apps\arduino-1.8.7\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ D:\Apps\arduino-1.8.7\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Apps\arduino-1.8.7\hardware\arduino\avr\variants\standard/pins_arduino.h
